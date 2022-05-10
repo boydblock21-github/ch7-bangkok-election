1bacd5cf93508d5e6394d6a999279b3631078254
nutchari@ch7.com <>
2022-05-10 14:24:03 +0700

    [Edit] Position map

--- js/map-hover-detail.js
+++ js/map-hover-detail.js
@@ -2,7 +2,6 @@
 
 $(".map-item").hover(function(e) {
     $(".seemore-county-container").css("display", "block");
-    // $(".seemore-county-container").html($(this).data("info"));
 });
 
 $(".map-item").mouseleave(function(e) {
@@ -12,49 +11,19 @@ $(".map-item").mouseleave(function(e) {
 
 $(document)
   .mousemove(function(e) {
-    $(".seemore-county-container").css("top", e.clientY - 580);
-    $(".seemore-county-container").css("right", e.clientX);
-    $(".seemore-county-container").css("left", e.clientX - 120);
-    // if(screen.width <= 576){
-    //   $(".seemore-county-container").css("top", e.clientY - 70);
-    //   $(".seemore-county-container").css("right", e.clientX);
-    //   $(".seemore-county-container").css("left", e.clientX - 40);
-    // }
-    // else if(screen.width >= 576) {
-    //   $(".seemore-county-container").css("top", e.clientY - 80);
-    //   $(".seemore-county-container").css("right", e.clientX);
-    //   $(".seemore-county-container").css("left", e.clientX - 100);
-    // }
-    // else if(screen.width >= 768) {
-    //   $(".seemore-county-container").css("top", e.clientY - 10);
-    //   $(".seemore-county-container").css("right", e.clientX);
-    //   $(".seemore-county-container").css("left", e.clientX - 100);
-    // }
-    // else if(screen.width >= 1024) {
-    //   $(".seemore-county-container").css("top", e.clientY - 480);
-    //   $(".seemore-county-container").css("right", e.clientX);
-    //   $(".seemore-county-container").css("left", e.clientX - 80);
-    // } 
-    // else if(screen.width >= 1360) {
-    //   $(".seemore-county-container").css("top", e.clientY - 480);
-    //   $(".seemore-county-container").css("right", e.clientX);
-    //   $(".seemore-county-container").css("left", e.clientX - 80);
-    // } 
-    // else {
-    //   $(".seemore-county-container").css("top", e.clientY - 450);
-    //   $(".seemore-county-container").css("right", e.clientX - 120);
-    //   $(".seemore-county-container").css("left", e.clientX);
-    // }
+    if(screen.width <= 576){
+      $(".seemore-county-container").css("top", e.offsetY - 70);
+      $(".seemore-county-container").css("right", e.offsetX);
+      $(".seemore-county-container").css("left", e.offsetX - 40);
+    }
+    else if(screen.width >= 576) {
+      $(".seemore-county-container").css("top", e.offsetY - 250);
+      $(".seemore-county-container").css("right", e.offsetX);
+      $(".seemore-county-container").css("left", e.offsetX - 50);
+    }
+   
   })
   .mouseover();
 
-  
-// var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-// if (ios) {
-//   $("a").on("click touchend", function() {
-//     var link = $(this).attr("href");
-//     window.open(link, "_blank");
-//     return false;
-//   });
-// }
+
   
