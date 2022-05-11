46e278fa2adc64b715c05914a44460141e42973c
nutchari@ch7.com <>
2022-05-11 14:10:16 +0700

    [Edit] Debug on safari

--- js/map-hover-detail.js
+++ js/map-hover-detail.js
@@ -3,14 +3,10 @@
 $(".map-item").hover(function(e) {
     $(".seemore-county-container").css("display", "block");
 });
-$("#mod-test").click(function(e) {
-    // $(".seemore-county-container").css("display", "none");
-    alert('mmm');
-});
 
-// $(".map-item").mouseleave(function(e) {
-//     $(".seemore-county-container").css("display", "none");
-// });
+$(".map-item").mouseleave(function(e) {
+    $(".seemore-county-container").css("display", "none");
+});
 
 
 $(document)
@@ -24,10 +20,49 @@ $(document)
       $(".seemore-county-container").css("top", e.offsetY - 250);
       $(".seemore-county-container").css("right", e.offsetX);
       $(".seemore-county-container").css("left", e.offsetX - 50);
-    }
-   
+    } 
   })
   .mouseover();
+
+
+
+
+function displaySeemoreOpenApp() {
+  var classArray = document.getElementsByClassName("seemore-county-container");
+  for (var i = 0; i < classArray.length; i++) {
+    classArray[i].className += " -hidden"
+  }
+}
+
+function OpenApp() {
+  var link = '';
+  var iOS = (navigator.userAgent.match(/(iPad|iPhone|iPod)/i) ? true : false);
+
+  if (iOS) {
+    return displaySeemoreOpenApp()
+  }
+
+  var ua = navigator.userAgent.toLowerCase();
+  var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+  if (isAndroid) {
+    return displaySeemoreOpenApp()
+  }
+
+  if( link != '' ){
+      $('.seemore-county-container').removeClass('-hidden');
+  } 
+}
+window.onload = function() {
+  OpenApp()
+};
+
+
+
+
+
+  
+  
+
 
 
   
